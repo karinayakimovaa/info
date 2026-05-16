cb976dce796522eba79c9756465c3735ca4d4bc6
karinayakimovaa <brifoes@gmail.com>
2026-05-16 18:38:59 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
 import './style.css'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
